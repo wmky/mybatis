d27506c9ee5f44d88aef3b245efd58291ee76894
kaikai <kaikai@juanpi.com>
2017-03-05 21:45:50 +0800

    perfect use db.properties ,but now I don't know how to use it correct.

--- mybatis-sample-master/src/test/java/com/example/mybatis/CritTest.java
+++ mybatis-sample-master/src/test/java/com/example/mybatis/CritTest.java
@@ -8,13 +8,14 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by wmky_kk on 2017/3/5.
  */
 public class CritTest {
     public static void main(String args[]) throws IOException{
-        InputStream inputStream = CritTest.class.getClassLoader().getResourceAsStream("mybatis/Configuration.xml");
+        InputStream inputStream = CritTest.class.getClassLoader().getResourceAsStream("mybatis/mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         // statement 由映射文件namespace + 标签
